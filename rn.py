0ba347c84bfcf5b68f8827511a937c1d3073bad8
alki22 <alki.collias@gmail.com>
2017-11-29 12:57:39 -0300

    New model

--- rn.py
+++ rn.py
@@ -1,66 +1,84 @@
-from __future__ import absolute_import, print_function, unicode_literals
+from __future__ import print_function
+
+from keras.models import Sequential
+from keras.layers.core import Dense, Activation, Dropout, Flatten
+from keras.layers.recurrent import LSTM
+from keras.utils.data_utils import get_file
 
 import numpy as np
 import random
-import re
 import sys
-import unicodedata
-import keras.backend as K
+import os
 
-from keras.models import Sequential
-from keras.layers import Dense, Dropout
-from keras.layers import LSTM, TimeDistributed
-from keras.optimizers import RMSprop
+path = "datasets/cuarteCorpus.txt"
+sys.stdout = open('output.txt','wt')
 
-filename = 'CuarteCorpus.txt'
-with open(filename, 'r') as file:
-	text = unicodedata.normalize('NFC', file.read()).lower()
-	text = re.sub('\s+', ' ', text).strip()
+try: 
+    text = open(path).read().lower()
+except UnicodeDecodeError:
+    import codecs
+    text = codecs.open(path, encoding='utf-8').read().lower()
 
-print('Corpus length:: %d' % len(text))
+print('corpus length:', len(text))
 
-# Get every different char in corpus
-chars = sorted(set(text))
+chars = set(text)
+words = set(open('datasets/cuarteCorpus.txt').read().lower().split())
 
-print('Total chars: %d' % len(chars))
+print("chars:",type(chars))
+print("words",type(words))
+print("total number of unique words",len(words))
+print("total number of unique chars", len(chars))
 
-char_index = dict((c, i) for i, c in enumerate(chars))
-index_char = dict((i, c) for i, c in enumerate(chars))
 
-K.clear_session()
+word_indices = dict((c, i) for i, c in enumerate(words))
+indices_word = dict((i, c) for i, c in enumerate(words))
 
-# Dimensions (Default: 64 and 40)
-hidden_layer_size = 64
-max_len = 30
+print("word_indices", type(word_indices), "length:",len(word_indices) )
+print("indices_words", type(indices_word), "length", len(indices_word))
 
-# Instantiate the model
+maxlen = 30
+step = 3
+print("maxlen:",maxlen,"step:", step)
+sentences = []
+next_words = []
+next_words= []
+sentences1 = []
+list_words = []
+
+sentences2=[]
+list_words=text.lower().split()
+
+
+for i in range(0,len(list_words)-maxlen, step):
+    sentences2 = ' '.join(list_words[i: i + maxlen])
+    sentences.append(sentences2)
+    next_words.append((list_words[i + maxlen]))
+print('nb sequences(length of sentences):', len(sentences))
+print("length of next_word",len(next_words))
+
+print('Vectorization...')
+X = np.zeros((len(sentences), maxlen, len(words)), dtype=np.bool)
+y = np.zeros((len(sentences), len(words)), dtype=np.bool)
+for i, sentence in enumerate(sentences):
+    for t, word in enumerate(sentence.split()):
+        X[i, t, word_indices[word]] = 1
+    y[i, word_indices[next_words[i]]] = 1
+
+
+print('Build model...')
+
 model = Sequential()
 
-# Add layers to the model
-model.add(LSTM(hidden_layer_size, input_shape=(max_len, len(chars)), return_sequences=True))
+model.add(LSTM(512, return_sequences=True, input_shape=(maxlen, len(words))))
 model.add(Dropout(0.2))
-model.add(TimeDistributed(Dense(len(chars), activation='softmax')))
+model.add(Flatten())
+model.add(Dense(len(words)))
+model.add(Activation('softmax'))
 
+model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
 
-model.compile(loss='categorical_crossentropy', optimizer='adam')
-model.summary()
-
-# Cut the text in sequences of max_len chars
-sentences = []
-next_chars = []
-
-for i in range(0, len(text) - max_len - 1, max_len):
-	sentences.append(text[i:i + max_len])
-	next_chars.append(text[i + 1 : i + max_len + 1])
-
-print('NB sequences:', len(sentences))
-
-X = np.zeros((len(sentences), max_len, len(chars)), dtype=np.bool)
-y = np.zeros((len(sentences), max_len, len(chars)), dtype=np.bool)
-for i, sentence in enumerate(sentences):
-	for t, char in enumerate(sentence):
-		X[i, t, char_index[char]] = 1
-		y[i, t, char_index[next_chars[i][t]]] = 1
+if os.path.isfile('GoTweights'):
+    model.load_weights('GoTweights')
 
 def sample(preds, temperature=1.0):
     # helper function to sample an index from a probability array
@@ -71,47 +89,36 @@ def sample(preds, temperature=1.0):
     probas = np.random.multinomial(1, preds, 1)
     return np.argmax(probas)
 
-def print_samples(model, sample_size=700):
-    start_index = random.randint(0, len(text) - max_len - 1)
 
-    for diversity in [0.2, 0.3, 0.5, 0.7, 0.8]:
-        print()
-        print('----- diversity:', diversity)
+# train the model, output generated text after each iteration
+for iteration in range(1, 300):
+    model.fit(X, y, batch_size=128, epochs=2)
+    model.save_weights('GoTweights',overwrite=True)
 
-        sentence = text[start_index: start_index + max_len]
-        print('----- Generating with seed: "' + sentence + '"')
-        sys.stdout.write(sentence)
+    start_index = random.randint(0, len(list_words) - maxlen - 1)
 
-        # Printing the sample
-        for i in range(sample_size):
-            x = np.zeros((1, max_len, len(chars)))
-            # Build the one-hot encoding for the sentence
-            for t, char in enumerate(sentence):
-                x[0, t, char_index[char]] = 1.
+    for diversity in [0.2, 0.5, 1.0, 1.2]:
+        generated = ''
+        sentence = list_words[start_index: start_index + maxlen]
+        generated += ' '.join(sentence)
+        sys.stdout.write(generated)
+        verse_length = random.randint(4,8)
+        for i in range(verse_length):
+            x = np.zeros((1, maxlen, len(words)))
+            for t, word in enumerate(sentence):
+                x[0, t, word_indices[word]] = 1.
 
-            preds = model.predict(x, verbose=0)[0][-1]
+            preds = model.predict(x, verbose=0)[0]
             next_index = sample(preds, diversity)
-            next_char = index_char[next_index]
-
-            sentence = sentence[1:] + next_char
-
-            sys.stdout.write(next_char)
+            next_word = indices_word[next_index]
+            generated += next_word
+            del sentence[0]
+            
+            verse_length = random.randint(4,8)
+            
+            sentence.append(next_word)
+            sys.stdout.write(' ')
+            sys.stdout.write(next_word)
             sys.stdout.flush()
         print()
-
-last_loss = -1.
-
-for iteration in range(1, 19):
-    print()
-    print('-' * 50)
-    print('Iteration', iteration)
-    history = model.fit(X, y,
-                        batch_size=128,
-                        epochs=40)
-
-    if iteration % 3 == 0:
-        print_samples(model)
-    if last_loss >= 0 and last_loss - history.history['loss'][0] < 0.001:
-        break
-    
-    last_loss = history.history['loss'][0]
+#model.save_weights('weights') 
