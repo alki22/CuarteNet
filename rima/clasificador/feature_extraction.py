d6f8dcdc01ad0062d9b63b85d7577ac35c53d5a7
alki22 <alki.collias@gmail.com>
2018-09-19 11:38:21 -0300

    New syllabification algorithm

--- rima/clasificador/feature_extraction.py
+++ rima/clasificador/feature_extraction.py
@@ -1,6 +1,4 @@
-import re
-
-from silabeo import Silabeo
+from silabas import silabas as separar
 
 _DIPTONGOS = {'ai', 'au', 'ei', 'eu', 'ia', 'ie', 'io', 'iu', 'oi', 'ou', 'ua',
 		 	  'ue', 'ui', 'uo'}
@@ -17,15 +15,14 @@ def esAguda(silabas):
 		(not any(a for a in _ACENTUADAS if a in silabas[-2]) and not silabas[-1][-1] in _NSOVOCAL))
 
 def acentuacion(palabra):
-	s = Silabeo()
-	silabas = s.parse(palabra)
+	silabas = separar(palabra)
 
 	for i in range(len(silabas)):
 		if any(a for a in _ACENTUADAS if a in silabas[i]):
-			return i, silabas[i]
+			return palabra, silabas, i, silabas[i]
 
 		if i >= len(silabas) - 1:
 			if esAguda(silabas):
-				return len(silabas) - 1, silabas[-1]
+				return palabra, silabas, len(silabas) - 1, silabas[-1]
 			else:
-				return len(silabas) - 2, silabas[-2]
+				return palabra, silabas, len(silabas) - 2, silabas[-2]
