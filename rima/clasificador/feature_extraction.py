47f615c4efee7b18e96f7fc0587c6e220077cd22
alki22 <alki.collias@gmail.com>
2018-10-02 18:12:06 -0300

    Added new case to esAguda and corrected case when it has grave accent for it only being when the word has two or more syllables

--- rima/clasificador/feature_extraction.py
+++ rima/clasificador/feature_extraction.py
@@ -1,3 +1,5 @@
+import pickle
+
 from silabas import silabas as separar
 
 _VOCALES = {'a', 'e', 'i', 'o', 'u'}
@@ -6,19 +8,30 @@ _ABIERTAS = {'a', 'e', 'o', 'á', 'é', 'ó'}
 
 _CERRADAS = {'i', 'u', 'í', 'ú'}
 
-_DIPTONGOS = {'ai', 'au', 'ei', 'eu', 'ia', 'ie', 'io', 'iu', 'oi', 'ou', 'ua',
-		 	  'ue', 'ui', 'uo'}
+_DIPTONGOS = {'ai', 'au', 'ei', 'eu', 'ia', 'ie', 'io', 
+			  'iu', 'oi', 'ou', 'ua', 'ue', 'ui', 'uo'}
 
 _NSOVOCAL = {'a', 'e', 'i', 'o', 'u', 'n', 's'}
 
 _ACENTUADAS = {'á', 'é', 'í', 'ó', 'ú'}
 
 def tieneDiptongo(palabra):
+	"""Devuelve si la palabra es aguda,
+	   dado que no es esdrújula o sobreesdrújula"""
 	return any([d for d in _DIPTONGOS if d in palabra])
 
 def esAguda(silabas):
-	return ((silabas[-1][-1] in _NSOVOCAL and any([a for a in _ACENTUADAS if a in silabas[-1]])) or 
-		(not any([a for a in _ACENTUADAS if a in silabas[-2]]) and not silabas[-1][-1] in _NSOVOCAL))
+	if len(silabas) == 1:
+		return True
+
+	terminaNSVocal = silabas[-1][-1] in _NSOVOCAL
+	tieneAcentoAgudo = any([a for a in _ACENTUADAS if a in silabas[-1]])
+	
+	if len(silabas) >= 2:
+		tieneAcentoGrave = any([a for a in _ACENTUADAS if a in silabas[-2]])
+
+	return ((terminaNSVocal and tieneAcentoAgudo) or 
+		(not tieneAcentoGrave and not tieneAcentoAgudo))
 
 def silabaTonica(silabas):
 
@@ -36,7 +49,7 @@ def vocalTonica(silabas, tonica):
 	silabaTonica = silabas[tonica]
 	vocalAbierta = [a for a in _ABIERTAS if a in silabaTonica]
 	vocalCerrada = [c for c in _CERRADAS if c in silabaTonica]
-	
+	print(silabas)
 	if any(vocalAbierta):
 		return vocalAbierta[0]
 	else:
@@ -47,7 +60,7 @@ def sigTonica(silabas, tonica):
 	posicion = silabas[tonica].index(vocTonica)
 	
 	if 0 <= posicion < len(silabas[tonica]) - 1:
-		return silabas[vocTonica][posicion + 1]
+		return silabas[tonica][posicion + 1]
 	else:
 		if tonica < len(silabas) - 1:
 			return silabas[tonica + 1][0]
@@ -76,6 +89,44 @@ def features(palabra):
 	silabas = separar(palabra)
 	tonica = silabaTonica(silabas)
 	
-	return (vocalTonica(silabas, tonica), sigTonica(silabas, tonica), 
-			antTonica(silabas, tonica), vocalesPostonicas(silabas, tonica), 
-			tieneDiptongo(palabra))
+	features = [vocalTonica(silabas, tonica), sigTonica(silabas, tonica), 
+				antTonica(silabas, tonica), vocalesPostonicas(silabas, tonica), 
+				tieneDiptongo(palabra)]
+
+	return features
+
+def diccDeFeatures(texto):
+	with open(texto, 'rb') as archivo:
+		dataset = pickle.load(archivo)
+
+	corpus = []
+	
+	for triple in dataset:
+		featuresPrimera = features(triple[0])
+		featuresSegunda = features(triple[1])
+
+		triple_features = {
+							# Features de la primera palabra de la 3-upla
+							'termPrimera' : triple[0],
+							'vocalTonicaPrimera' : featuresPrimera[0],
+							'sigTonicaPrimera' : featuresPrimera[1],
+							'antTonicaPrimera' : featuresPrimera[2],
+							'postonicasPrimera' : featuresPrimera[3],
+							'diptongoPrimera' : featuresPrimera[4],
+							
+							# Features de la segunda palabra de la 3-upla
+							'termSegunda' : triple[1],
+							'vocalTonicaSegunda' : featuresSegunda[0],
+							'sigTonicaSegunda' : featuresSegunda[1],
+							'antTonicaSegunda' : featuresSegunda[2],
+							'postonicasSegunda' : featuresSegunda[3],
+							'diptongoSegunda' : featuresSegunda[4],
+							
+							# ¿Riman las dos palabras?
+							'riman' : triple[2]
+						  }
+		corpus.append(triple_features)
+
+	return corpus
+
+#print(diccDeFeatures("../dataset/dataset.pk"))
